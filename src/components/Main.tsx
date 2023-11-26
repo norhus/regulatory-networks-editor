c65bc34f0c70be192f7117e63890b1f83740e207
Norbert Husarcik <husarcik.norbert@easycon.cz>
2023-11-26 12:19:23 +0100

    fix: Remove event listeners on unmount

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,51 +4,50 @@ import Menu from "./menu/Menu"
 import cytoscape from "cytoscape"
 
 const Main: React.FC = () => {
-    const [nodes, setNodes] = useState<cytoscape.NodeDefinition[]>([
-        { data: { id: "1", label: "Node 1" }, position: { x: 0, y: 0 } },
-        { data: { id: "2", label: "Node 2" }, position: { x: 100, y: 0 } }
-    ])
-    const [edges, setEdges] = useState<cytoscape.EdgeDefinition[]>([
-        { data: { source: "1", target: "2", label: "Edge from Node1 to Node2" }}
-    ])
-    const [selectedNodes, setSelectedNodes] = useState<string[]>([])
-    console.log(edges)
+	const [nodes, setNodes] = useState<cytoscape.NodeDefinition[]>([
+		{ data: { id: "1", label: "Node1" }, position: { x: 0, y: 0 } },
+		{ data: { id: "2", label: "Node2" }, position: { x: 100, y: 0 } }
+	])
+	const [edges, setEdges] = useState<cytoscape.EdgeDefinition[]>([
+		{ data: { source: "1", target: "2", label: "Edge from Node1 to Node2" }}
+	])
+	const [selectedNodes, setSelectedNodes] = useState<string[]>([])
 
-    const handleSelectNode = (id: string, selected: boolean) => {
-        if (selected) {
-            setSelectedNodes([...selectedNodes, id])
-        } else {
-            setSelectedNodes(selectedNodes.filter((node) => node !== id))
-        }
-    }
+	const handleSelectNode = (id: string, selected: boolean) => {
+		if (selected) {
+			setSelectedNodes([...selectedNodes, id])
+		} else {
+			setSelectedNodes(selectedNodes.filter((node) => node !== id))
+		}
+	}
 
-    const resetSelected = () => {
-        setSelectedNodes([])
-    }
+	const resetSelected = () => {
+		setSelectedNodes([])
+	}
 
-    const addEdge = () => {
+	const addEdge = () => {
         if (selectedNodes.length === 2) {
-            const newEdge: cytoscape.EdgeDefinition = { data: { source: selectedNodes[0], target: selectedNodes[1],
-                    label: `Edge from Node ${selectedNodes[0]} to Node ${selectedNodes[1]}` }}
-            setEdges([...edges, newEdge])
-        }
-    }
+			const newEdge: cytoscape.EdgeDefinition = { data: { source: selectedNodes[0], target: selectedNodes[1],
+					label: `Edge from Node${selectedNodes[0]} to Node${selectedNodes[1]}` }}
+			setEdges([...edges, newEdge])
+		}
+	}
 
-    return (
-      <>
-        <Menu
-            addEdge={addEdge}
-        />
-          <Board
-              nodes={nodes}
-              edges={edges}
-              setNodes={setNodes}
-              setEdges={setEdges}
-              onSelectNode={handleSelectNode}
-              resetSelected={resetSelected}
+	return (
+        <>
+          <Menu
+			addEdge={addEdge}
           />
-      </>
-  )
+          <Board
+			  nodes={nodes}
+			  edges={edges}
+			  setNodes={setNodes}
+			  setEdges={setEdges}
+			  onSelectNode={handleSelectNode}
+			  resetSelected={resetSelected}
+		  />
+	    </>
+    )
 };
 
 export default Main
--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import CytoscapeComponent from "react-cytoscapejs"
 import cytoscape from "cytoscape"
 import "./Board.css"
@@ -20,37 +20,38 @@ const Board: React.FC<Props> = ({
     onSelectNode,
     resetSelected
 }) => {
+    const [cyBoard, setCyBoard] = useState<cytoscape.Core | null>(null)
+
+    useEffect(() => {
+        cyBoard?.selectionType("additive")
+        cyBoard?.on("dblclick", (e) => {
+            if (e.target === cyBoard) {
+                const id: string = (cyBoard?.nodes().length ? cyBoard.nodes().length + 1 : 1).toString()
+                setNodes([...nodes, { data: { id: id, label: `Node${id}` }, position: e.position }])
+            }
+        })
+
+        cyBoard?.on("click", (e) => {
+            if (e.target === cyBoard) {
+                resetSelected()
+            } else if (e.target.isNode()) {
+                if (e.target.selected()) {
+                    onSelectNode(e.target.data().id, false)
+                } else {
+                    onSelectNode(e.target.data().id, true)
+                }
+            }
+        })
+        return () => {
+            cyBoard?.removeAllListeners()
+        }
+    }, [cyBoard, nodes, onSelectNode, resetSelected, setNodes])
 
     return (
         <CytoscapeComponent
             key={"cyBoard"}
             id={"cyBoard"}
-            cy={(cy) => {
-                cy.selectionType("additive")
-                cy?.on("dblclick", (e) => {
-                    if (e.target === cy) {
-                        const id: string = (cy.nodes().length + 1).toString()
-                        // cy.add({
-                        //     group: "nodes",
-                        //     data: { id: id, label: `Node ${id}` },
-                        //     position: e.position
-                        // })
-                        setNodes([...nodes, { data: { id: id, label: `Node ${id}` }, position: e.position }])
-                    }
-                })
-
-                cy?.on("click", (e) => {
-                    if (e.target === cy) {
-                        resetSelected()
-                    } else if (e.target.isNode()) {
-                        if (e.target.selected()) {
-                            onSelectNode(e.target.data().id, false)
-                        } else {
-                            onSelectNode(e.target.data().id, true)
-                        }
-                    }
-                })
-            }}
+            cy={(cy) => {setCyBoard(cy)}}
             className={"board"}
             elements={
                 CytoscapeComponent.normalizeElements({nodes, edges})
